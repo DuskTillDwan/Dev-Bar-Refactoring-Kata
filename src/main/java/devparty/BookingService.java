79b4bb05323853d120d2cd486e3d7eb9d305ba92
Taylor Williams <dwan.williams.t@gmail.com>
2025-02-20 15:57:44 -0600

    . r separated for loop return and logic. extracted firstAvailableBoat method.

--- src/main/java/devparty/BookingService.java
+++ src/main/java/devparty/BookingService.java
@@ -2,6 +2,7 @@ package devparty;
 
 import devparty.model.Bar;
 import devparty.model.BarData;
+import devparty.model.BoatData;
 import devparty.model.BookingData;
 
 import java.time.DayOfWeek;
@@ -57,17 +58,16 @@ public class BookingService {
                 .orElse(null);
 
         for (var boatData : boats) {
-            Bar bar = new Bar();
-            if (bar.hasEnoughCapacity(boatData, maxNumberOfDevs)) {
+            if (new Bar().hasEnoughCapacity(boatData, maxNumberOfDevs)) {
                 String name = boatData.getName();
                 System.out.println("Bar booked: " + name + " at " + bestDate);
                 BarData barData = new BarData(boatData.getName(), boatData.getMaxPeople(), allDays());
                 bookingRepo.save(new BookingData(
                         barData, bestDate
                 ));
-                return true;
             }
         }
+        if (findFirstAvailableBoat(boats, maxNumberOfDevs)) return true;
 
         Optional<BarData> firstAvailableBar = findFirstAvailableBar(bars, maxNumberOfDevs, bestDate);
 
@@ -75,6 +75,15 @@ public class BookingService {
                 .ifPresent(barData -> printAndSaveBooking(barData, bestDate));
 
         return firstAvailableBar.isPresent();
+    }
+
+    private static boolean findFirstAvailableBoat(List<BoatData> boats, int maxNumberOfDevs) {
+        for (var boatData : boats) {
+            if (new Bar().hasEnoughCapacity(boatData, maxNumberOfDevs)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void printAndSaveBooking(BarData barData, LocalDate bestDate) {
